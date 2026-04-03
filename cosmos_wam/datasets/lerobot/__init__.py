5c8f628be0db2f02e2f06e13761f3e5ea889b02c
SpriteWithoutIce <22373442@buaa.edu.cn>
2026-04-03 20:18:15 +0800

    local

--- cosmos_wam/datasets/lerobot/__init__.py
+++ cosmos_wam/datasets/lerobot/__init__.py
@@ -1,1 +1,5 @@
 # LeRobot dataset support
+from .robot_video_dataset import RobotVideoDataset
+from .base_lerobot_dataset import BaseLerobotDataset
+
+__all__ = ["RobotVideoDataset", "BaseLerobotDataset"]
