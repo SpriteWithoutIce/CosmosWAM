8e87d5ffbc6babf2bdc2525d185efedcfc76f925
SpriteWithoutIce <22373442@buaa.edu.cn>
2026-04-06 13:17:50 +0800

    lerobot dataset

--- cosmos_wam/datasets/lerobot/processors/fastwam_processor.py
+++ cosmos_wam/datasets/lerobot/processors/fastwam_processor.py
@@ -224,7 +224,7 @@ class FastWAMProcessor(BaseProcessor):
             for trans in current_transforms:
                 image = trans(image)
             
-            meta_shape = [self.num_obs_steps] + shape
+            meta_shape = tuple([self.num_obs_steps] + shape)
             assert image.shape == meta_shape, \
                 f"Expected shape {meta_shape}, got {image.shape} after transforms for key {key}"
 
