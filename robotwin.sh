eac5a080a24d4a5e2fab29b1da1356e16722acbc
llgsdsgll <gengshuang@zju.edu.cn>
2026-04-07 17:52:52 +0800

    bash

--- robotwin.sh
+++ robotwin.sh
@@ -1,9 +1,9 @@
 python experiments/robotwin/eval_robotwin_single.py \
     ckpt=/mnt/data/linyihan/ckpt/step_0002000_bf16.pt \
-    EVALUATION.task_name=adjust_bottle \
+    EVALUATION.task_name=beat_block_hammer \
     EVALUATION.dataset_stats_path=./dataset_stats.json \
     EVALUATION.robotwin_root=/root/linyihan/RoboTwin \
-    EVALUATION.fixed_text_embedding_path=/mnt/data/linyihan/ckpt/0aa37248d1da4ad461c558c5652997440e6bced3eb30752248000c9cc081774e.t5_len512.pt \
-    EVALUATION.num_inference_steps=4 \
-    EVALUATION.replan_steps=8 \
+    EVALUATION.fixed_text_embedding_path=/mnt/data/linyihan/text_embeds_cache/robotwin_reason1 \
+    EVALUATION.num_inference_steps=8 \
+    EVALUATION.replan_steps=4 \
     gpu_id=7
