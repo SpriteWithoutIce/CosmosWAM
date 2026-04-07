e506a2f905a4e6fa72b0405d8a02877f82ca0ec2
llgsdsgll <gengshuang@zju.edu.cn>
2026-04-07 16:52:25 +0800

    config

--- robotwin.sh
+++ robotwin.sh
@@ -2,5 +2,6 @@ python experiments/robotwin/eval_robotwin_single.py \
     ckpt=/mnt/data/linyihan/ckpt/step_0002000_bf16.pt \
     EVALUATION.task_name=adjust_bottle \
     EVALUATION.dataset_stats_path=./dataset_stats.json \
+    EVALUATION.robotwin_root=/root/linyihan/RoboTwin \
     EVALUATION.fixed_text_embedding_path=/mnt/data/linyihan/ckpt/0aa37248d1da4ad461c558c5652997440e6bced3eb30752248000c9cc081774e.t5_len512.pt \
     gpu_id=7
