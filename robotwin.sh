c9a9b1f5a9b89b89da1da0f2bdd2a1264443b719
llgsdsgll <gengshuang@zju.edu.cn>
2026-04-08 19:58:19 +0800

    robotwin test

--- robotwin.sh
+++ robotwin.sh
@@ -1,6 +1,6 @@
 python experiments/robotwin/eval_robotwin_single.py \
     ckpt=/mnt/data/linyihan/ckpt/step_0014000_bf16.pt \
-    EVALUATION.task_name=click_bell \
+    EVALUATION.task_name=adjust_bottle \
     EVALUATION.use_online_text_encoder=true \
     EVALUATION.dataset_stats_path=./dataset_stats.json \
     EVALUATION.robotwin_root=/root/linyihan/RoboTwin \
@@ -8,7 +8,7 @@ python experiments/robotwin/eval_robotwin_single.py \
     EVALUATION.text_encoder_device=cuda:0 \
     EVALUATION.device=cuda:1 \
     EVALUATION.num_inference_steps=20 \
-    EVALUATION.replan_steps=4 \
+    EVALUATION.replan_steps=8 \
     EVALUATION.instruction_type=seen \
     gpu_id=1
 
