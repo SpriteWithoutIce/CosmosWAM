88edfbea1ecff2efe66b62dfba2ff4198942885c
coke_and_1ce <22373442@buaa.edu.cn>
2026-04-08 15:37:35 +0800

    bash

---
+++ scripts/gaussian/run.sh
@@ -0,0 +1,7 @@
+python libero_heatmap_projection.py visualize_single \
+      --parquet_path /home/jwhe/linyihan/datasets/libero_mujoco3.3.2/libero_spatial_no_noops_lerobot/data/chunk-000/episode_000000.parquet \
+      --video_path /home/jwhe/linyihan/datasets/libero_mujoco3.3.2/libero_spatial_no_noops_lerobot/videos/chunk-000/observation.images.image/episode_000000.mp4 \
+      --camera_params camera_params.json \
+      --output heatmap_vis.mp4 \
+      --sigma 8.0 \
+      --alpha 0.5
