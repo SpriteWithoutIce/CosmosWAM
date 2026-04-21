2d5728a41527e9438e471643a92aaa6d051c6905
coke_and_1ce <22373442@buaa.edu.cn>
2026-04-21 14:59:46 +0800

    config

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ LOG_FILE="log/train_libero_$(date +%Y%m%d_%H%M%S).log"
 
 torchrun --nproc_per_node=2 scripts/train.py \
     --config-name train_cosmos_2b_libero \
-    # 2>&1 | tee "$LOG_FILE"
+    2>&1 | tee "$LOG_FILE"
